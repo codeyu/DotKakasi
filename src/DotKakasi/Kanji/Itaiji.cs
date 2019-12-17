3f7665fa0d51ee3b34e23e250389fe1cae79a847
codeYu <codeyu@gmail.com>
2019-12-17 15:03:02 +0900

    Update Itaiji.cs

--- src/DotKakasi/Kanji/Itaiji.cs
+++ src/DotKakasi/Kanji/Itaiji.cs
@@ -1,10 +1,7 @@
-using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.IO.Compression;
-using System.Reflection;
-
+using DotKakasi.Common;
+using DotKakasi.Scripts;
 namespace DotKakasi.Kanji
 {
     public class Itaiji
