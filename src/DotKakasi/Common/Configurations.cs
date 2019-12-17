8fa334b730f85e5aa9cfd51fbe6c8300de7f5f22
codeYu <codeyu@gmail.com>
2019-12-17 14:22:18 +0900

    Update Configurations.cs

--- src/DotKakasi/Common/Configurations.cs
+++ src/DotKakasi/Common/Configurations.cs
@@ -2,14 +2,14 @@ namespace DotKakasi.Common
 {
     public class Configurations
     {
-        public string jisyo_hepburn_hira = "hepburnhira.json.gz";
-        public string jisyo_passport_hira = "passporthira.json.gz";
-        public string jisyo_kunrei_hira = "kunreihira.json.gz";
-        public string jisyo_itaiji = "itaijidict.json.gz";
-        public string jisyo_kanwa = "kanwadict.json.gz";
-        public string jisyo_hepburn = "hepburndict.json.gz";
-        public string jisyo_passport = "passportdict.json.gz";
-        public string jisyo_kunrei = "kunreidict.json.gz";
+        public const string jisyo_hepburn_hira = "hepburnhira.json.gz";
+        public const string jisyo_passport_hira = "passporthira.json.gz";
+        public const string jisyo_kunrei_hira = "kunreihira.json.gz";
+        public const string jisyo_itaiji = "itaijidict.json.gz";
+        public const string jisyo_kanwa = "kanwadict.json.gz";
+        public const string jisyo_hepburn = "hepburndict.json.gz";
+        public const string jisyo_passport = "passportdict.json.gz";
+        public const string jisyo_kunrei = "kunreidict.json.gz";
 
         public static string dictpath()
         {
